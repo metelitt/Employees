7e14c95b97db71c813d851c971d0d7186600f784
metelitt <104203274+metelitt@users.noreply.github.com>
2023-11-10 21:04:25 +0300

    Backend

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const {register,login,current}=require('../controllers/users')
+/* /api/user/login*/
+router.post('/login', login)
+/* /api/user/register*/
+router.post('/register',register)
+/* /api/user/current*/
+router.get('/current',current),
+
+module.exports = router;
